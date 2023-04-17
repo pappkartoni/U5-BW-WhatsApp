66fd271c3e84bb37f95e83a2f3a779e3732aeb72
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-04-17 17:05:43 +0200

    added avatar saved on google login

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,120 +1,131 @@
-import jwt from "jsonwebtoken"
-import UsersModel from "../api/users/model.js"
-import createHttpError from "http-errors"
-import GoogleStrategy from "passport-google-oauth20"
-import multer from "multer"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
-import { v2 as cloudinary } from "cloudinary"
+import jwt from "jsonwebtoken";
+import UsersModel from "../api/users/model.js";
+import createHttpError from "http-errors";
+import GoogleStrategy from "passport-google-oauth20";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-export const createAccessToken = payload => 
-    new Promise((resolve, reject) => 
-        jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "15m"}, (err, token) => {
-            if (err) reject(err)
-            else resolve(token)
-        })
+export const createAccessToken = (payload) =>
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" },
+      (err, token) => {
+        if (err) reject(err);
+        else resolve(token);
+      }
     )
+  );
 
-export const verifyAccessToken = token =>
-    new Promise((resolve, reject) =>
-        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-            if (err) reject(err)
-            else resolve(payload)
-        })
+export const verifyAccessToken = (token) =>
+  new Promise((resolve, reject) =>
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err) reject(err);
+      else resolve(payload);
+    })
+  );
+
+export const createRefreshToken = (payload) =>
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      process.env.REFRESH_SECRET,
+      { expiresIn: "15m" },
+      (err, token) => {
+        if (err) reject(err);
+        else resolve(token);
+      }
     )
+  );
 
-export const createRefreshToken = payload => 
-    new Promise((resolve, reject) => 
-        jwt.sign(payload, process.env.REFRESH_SECRET, {expiresIn: "15m"}, (err, token) => {
-            if (err) reject(err)
-            else resolve(token)
-        })
-    )
+export const verifyRefreshToken = (token) =>
+  new Promise((resolve, reject) =>
+    jwt.verify(token, process.env.REFRESH_SECRET, (err, payload) => {
+      if (err) reject(err);
+      else resolve(payload);
+    })
+  );
 
-export const verifyRefreshToken = token =>
-    new Promise((resolve, reject) =>
-        jwt.verify(token, process.env.REFRESH_SECRET, (err, payload) => {
-            if (err) reject(err)
-            else resolve(payload)
-        })
-    )
+export const createTokens = async (user) => {
+  const accessToken = await createAccessToken({ _id: user._id });
+  const refreshToken = await createRefreshToken({ _id: user._id });
 
-export const createTokens = async user => {
-    const accessToken = await createAccessToken({_id: user._id})
-    const refreshToken = await createRefreshToken({_id: user._id})
+  user.refreshToken = refreshToken;
+  await user.save();
 
-    user.refreshToken = refreshToken
-    await user.save()
+  return { accessToken, refreshToken };
+};
 
-    return {accessToken, refreshToken}
-}
-
-export const verifyAndRefreshTokens = async currentRefreshToken => {
-    try {
-        const {_id} = await verifyRefreshToken(currentRefreshToken)
-        const user = await UsersModel.findById(_id)
-        if (!user) throw new createHttpError(404, `User with id ${_id} not found.`)
-        if (user.refreshToken && user.refreshToken === currentRefreshToken) {
-            const {accessToken, refreshToken} = await createTokens(user)
-            return {accessToken, refreshToken}
-        } else {
-            throw new createHttpError(401, "Invalid refresh token.")
-        }
-    } catch (error) {
-        throw new createHttpError(401, "Please log in.")
+export const verifyAndRefreshTokens = async (currentRefreshToken) => {
+  try {
+    const { _id } = await verifyRefreshToken(currentRefreshToken);
+    const user = await UsersModel.findById(_id);
+    if (!user) throw new createHttpError(404, `User with id ${_id} not found.`);
+    if (user.refreshToken && user.refreshToken === currentRefreshToken) {
+      const { accessToken, refreshToken } = await createTokens(user);
+      return { accessToken, refreshToken };
+    } else {
+      throw new createHttpError(401, "Invalid refresh token.");
     }
-}
+  } catch (error) {
+    throw new createHttpError(401, "Please log in.");
+  }
+};
 
 export const jwtAuth = async (req, res, next) => {
-    if (!req.headers.authorization) {
-        next(createHttpError(401, "No bearer token provided. 🐻"))
-    } else {
-        const accessToken = req.headers.authorization.replace("Bearer ", "")
-        try {
-            const payload = await verifyAccessToken(accessToken)
-            req.user = {_id: payload._id}
-            next()
-        } catch (error) {
-            console.log(error)
-            next(createHttpError(401, "Invalid token."))
-        }
+  if (!req.headers.authorization) {
+    next(createHttpError(401, "No bearer token provided. 🐻"));
+  } else {
+    const accessToken = req.headers.authorization.replace("Bearer ", "");
+    try {
+      const payload = await verifyAccessToken(accessToken);
+      req.user = { _id: payload._id };
+      next();
+    } catch (error) {
+      console.log(error);
+      next(createHttpError(401, "Invalid token."));
     }
-}
+  }
+};
 
 export const googleStrategy = new GoogleStrategy(
-    {
-        clientID: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: `${process.env.BE_URL}/users/googlecallback`
-    },
-    async (_, __, profile, pnext) => {
-        try {
-            const {email, given_name, family_name, sub} = profile._json
-            const user = await UsersModel.findOne({email})
-            if (user) {
-                const {accessToken, refreshToken} = await createTokens(user)
-                pnext(null, {accessToken, refreshToken})
-            } else {
-                const newUser = await UsersModel({
-                    name: given_name + " " + family_name,
-                    email,
-                    googleId: sub
-                })
+  {
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: `${process.env.BE_URL}/users/googlecallback`,
+  },
+  async (_, __, profile, pnext) => {
+    try {
+      const { email, given_name, family_name, sub, picture } = profile._json;
+      const user = await UsersModel.findOne({ email });
+      if (user) {
+        const { accessToken, refreshToken } = await createTokens(user);
+        pnext(null, { accessToken, refreshToken });
+      } else {
+        const newUser = await UsersModel({
+          name: given_name + " " + family_name,
+          email,
+          avatar: picture,
+          googleId: sub,
+        });
 
-                const created = newUser.save()
-                const {accessToken, refreshToken} = createTokens(created)
-                pnext(null, {accessToken, refreshToken})
-            }
-        } catch (error) {
-            pnext(error)
-        }
+        const created = newUser.save();
+        const { accessToken, refreshToken } = createTokens(created);
+        pnext(null, { accessToken, refreshToken });
+      }
+    } catch (error) {
+      pnext(error);
     }
-)
+  }
+);
 
 export const cloudinaryUploader = multer({
-    storage: new CloudinaryStorage({
-        cloudinary,
-        params: {
-            folder: "u5/whatsapp/avatars"
-        }
-    })
-}).single("avatar")
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "u5/whatsapp/avatars",
+    },
+  }),
+}).single("avatar");
