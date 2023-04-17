a0309862669e6a925a83e476d20c00cf4a77b022
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-04-17 12:48:13 +0200

    get done

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,38 +1,31 @@
-import express from "express"
-import createError from "http-errors"
-import UsersModel from "./model.js"
+import express from "express";
+import createError from "http-errors";
+import UsersModel from "./model.js";
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
-usersRouter.get("/", async (req, res, next) => {
+usersRouter.get("/", async (req, res, next) => {});
 
-})
+usersRouter.get("/me", async (req, res, next) => {});
 
-usersRouter.get("/me", async (req, res, next) => {
-    
-})
+usersRouter.put("/me", async (req, res, next) => {});
 
-usersRouter.put("/me", async (req, res, next) => {
-
-})
-
-usersRouter.post("/me/avatar", async (req, res, next) => {
-
-})
+usersRouter.post("/me/avatar", async (req, res, next) => {});
 usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const users = await UsersModel.findById(req.params.userId);
+    if (users) {
+      res.send(users);
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.post("/account", async (req, res, next) => {});
+usersRouter.post("/session", async (req, res, next) => {});
+usersRouter.delete("/session", async (req, res, next) => {});
+usersRouter.post("/session/refresh", async (req, res, next) => {});
 
-})
-usersRouter.post("/account", async (req, res, next) => {
-
-})
-usersRouter.post("/session", async (req, res, next) => {
-
-})
-usersRouter.delete("/session", async (req, res, next) => {
-
-})
-usersRouter.post("/session/refresh", async (req, res, next) => {
-
-})
-
-export default usersRouter
+export default usersRouter;
